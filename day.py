8847088fb0eb45d4f4487eedb1b1a5418ce708f1
Anitudh95 <Anirudha3126@gmail.com>
2025-06-26 12:37:56 +0530

    Day-1: Basic python

---
+++ day.py
@@ -0,0 +1,17 @@
+# Greeting
+print("Hello, welcome to new journey of Python programming!")
+
+# User details
+name = input("enter your name: ")
+age = int(input("Enter your age: "))
+print(f"Hello {name}, you are {age} years old.\n welcome to python")
+
+# Data type
+pi = 3.14
+ch = "ajay"
+num = 32
+com = 3 + 7j
+print(f"pi is of type {type(pi)}")
+print(f"ch is of type {type(ch)}")
+print(f"num is of type {type(num)}")
+print(f"com is of type {type(com)}")
